67d9038ae600c6fc652495b54a11c6a845f07ae6
Bartosz Butrym <butrymbartosz@gmail.com>
2023-08-11 08:06:27 +0200

    node app with express, mongodb and mongoose

---
+++ controllers/userController.js
@@ -0,0 +1,34 @@
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+
+  res
+    .status(200)
+    .json({ status: 'success', results: users.length, data: { users } });
+});
+
+exports.getUser = (req, res) => {
+  res
+    .status(500)
+    .json({ status: 500, message: 'This route is not yet defined!' });
+};
+
+exports.createUser = (req, res) => {
+  res
+    .status(500)
+    .json({ status: 500, message: 'This route is not yet defined!' });
+};
+
+exports.updateUser = (req, res) => {
+  res
+    .status(500)
+    .json({ status: 500, message: 'This route is not yet defined!' });
+};
+
+exports.deleteUser = (req, res) => {
+  res
+    .status(500)
+    .json({ status: 500, message: 'This route is not yet defined!' });
+};
